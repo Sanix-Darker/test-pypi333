f3cc84d6b2cd81e10eb9a809eaa934e72eeea2de
sanix-darker <s4nixd@gmail.com>
2022-03-21 16:33:47 +0100

    ddd: fff

--- .github/workflows/test-pypi.sh
+++ .github/workflows/test-pypi.sh
@@ -12,32 +12,6 @@ SETUP_FILE="$GITHUB_WORKSPACE/setup.py"
 TMP_VERSION=$(date +%s)
 REPOSITORY_NAME=$(echo "$GITHUB_REPOSITORY" | awk -F / '{print $2}')
 
-if [ -f "$TOM_FILE" ]; then
-
-     echo "[-] $TOM_FILE exists, poetry install/build"
-
-     # installation of poetry
-     pip install -U poetry
-
-     sed -i 's/^.*version.*=.*$/version = '\"$TMP_VERSION\"'/' $TOM_FILE
-     # the build process
-     poetry build
-elif [ -f "$SETUP_FILE" ]; then
-
-     echo "[-] $SETUP_FILE exists, normal sdist/wheel build"
-
-     pip install wheel
-
-     sed -i 's/^.*version.*=.*$/version='$TMP_VERSION',/' $SETUP_FILE
-     # The build process
-     python $SETUP_FILE sdist bdist_wheel
-else
-
-     echo "[x] No $TOM_FILE nor $SETUP_FILE, will stop here !"
-     exit 1
-
-fi
-
 # The upload/publish process
 pip install twine
 
@@ -54,8 +28,37 @@ repository = https://test.pypi.org/legacy/
 
 EOF
 
-# the upload process
-twine upload -r testpypi "$GITHUB_WORKSPACE/dist/*"
+if [ -f "$TOM_FILE" ]; then
+
+     echo "[-] $TOM_FILE exists, poetry install/build"
+
+     # installation of poetry
+     pip install -U poetry
+
+     sed -i 's/^.*version.*=.*$/version = '\"$TMP_VERSION\"'/' $TOM_FILE
+     # the build process
+     poetry build
+
+     # the upload process
+     twine upload -r testpypi "$GITHUB_WORKSPACE/dist/*"
+elif [ -f "$SETUP_FILE" ]; then
+
+     echo "[-] $SETUP_FILE exists, normal sdist/wheel build"
+
+     pip install wheel
+
+     sed -i 's/^.*version.*=.*$/version='$TMP_VERSION',/' $SETUP_FILE
+     # The build process
+     python $SETUP_FILE sdist bdist_wheel
+
+     twine upload -r testpypi dist/*
+else
+
+     echo "[x] No $TOM_FILE nor $SETUP_FILE, will stop here !"
+     exit 1
+
+fi
+
 
 # we set output vars for next steps
 echo "::set-output name=PKG::$(echo "$REPOSITORY_NAME==$TMP_VERSION")"
