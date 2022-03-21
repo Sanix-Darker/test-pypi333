78d57fc3f4ed889a7d0287b0cdaad0b1ed695ee4
darker <s4nixd@gmail.com>
2022-03-21 16:35:34 +0100

    xdfff: aassssss (#2)
    * xdfff: aassssss
    
    * dddd: ttyyy
    
    * ddd: fff

--- .github/workflows/test-pypi.sh
+++ .github/workflows/test-pypi.sh
@@ -10,33 +10,13 @@ pip install -U pip
 TOM_FILE="$GITHUB_WORKSPACE/pyproject.toml"
 SETUP_FILE="$GITHUB_WORKSPACE/setup.py"
 TMP_VERSION=$(date +%s)
+REPOSITORY_NAME=$(echo "$GITHUB_REPOSITORY" | awk -F / '{print $2}')
 
-if [ -f "$TOM_FILE" ]; then
+# The upload/publish process
+pip install twine
 
-     echo "[-] $TOM_FILE exists, poetry install/build"
-
-     # installation of poetry
-     pip install -U poetry
-     # configuration of the repository-url where the build will be pushed
-     poetry config repositories.test-pypi https://test.pypi.org/legacy/
-     poetry config pypi-token.pypi $TEST_PYPI_TOKEN
-
-     sed -i 's/^.*version.*=.*$/version = '$TMP_VERSION',/' $TOM_FILE
-
-     # We publish
-     poetry publish --build
-
-elif [ -f "$SETUP_FILE" ]; then
-
-     echo "[-] $SETUP_FILE exists, normal sdist/wheel build"
-
-     pip install wheel twine
-
-     sed -i 's/^.*version.*=.*$/version='$TMP_VERSION',/' $SETUP_FILE
-
-     python $SETUP_FILE sdist bdist_wheel
-     # we create our credential file for username, password and repository url
-     cat <<EOF > ~/.pypirc
+# we create our credential file for username, password and repository url
+cat <<EOF > ~/.pypirc
 [distutils]
 index-servers =
   testpypi
@@ -47,9 +27,31 @@ password = $TEST_PYPI_TOKEN
 repository = https://test.pypi.org/legacy/
 
 EOF
-     # the upload process
-     twine upload -r testpypi dist/*
 
+if [ -f "$TOM_FILE" ]; then
+
+     echo "[-] $TOM_FILE exists, poetry install/build"
+
+     # installation of poetry
+     pip install -U poetry
+
+     sed -i 's/^.*version.*=.*$/version = '\"$TMP_VERSION\"'/' $TOM_FILE
+     # the build process
+     poetry build
+
+     # the upload process
+     twine upload -r testpypi "$GITHUB_WORKSPACE/dist/*"
+elif [ -f "$SETUP_FILE" ]; then
+
+     echo "[-] $SETUP_FILE exists, normal sdist/wheel build"
+
+     pip install wheel
+
+     sed -i 's/^.*version.*=.*$/version='$TMP_VERSION',/' $SETUP_FILE
+     # The build process
+     python $SETUP_FILE sdist bdist_wheel
+
+     twine upload -r testpypi dist/*
 else
 
      echo "[x] No $TOM_FILE nor $SETUP_FILE, will stop here !"
@@ -57,7 +59,8 @@ else
 
 fi
 
-REPOSITORY_NAME=$(echo "$GITHUB_REPOSITORY" | awk -F / '{print $2}')
+
+# we set output vars for next steps
 echo "::set-output name=PKG::$(echo "$REPOSITORY_NAME==$TMP_VERSION")"
 echo "::set-output name=MSG::$(echo "https://test.pypi.org/project/$REPOSITORY_NAME/$TMP_VERSION/")"
 
